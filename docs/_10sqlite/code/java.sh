00261c20b64159e64a54966875d38dd256005142
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-09-11 15:49:17 +0200

    renamed

--- docs/_10sqlite/code/java.sh
+++
@@ -1,7 +0,0 @@
-FILE=sqlite-jdbc-3.36.0.3.jar
-if [ -f "$FILE" ]; then
-    echo "$FILE exists."
-else
-    echo "$FILE does not exist."
-    curl -L -H "Accept: application/zip" https://github.com/xerial/sqlite-jdbc/releases/download/3.36.0.3/sqlite-jdbc-3.36.0.3.jar -o sqlite-jdbc-3.36.0.3.jar
-fi
